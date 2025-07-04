07a85c79cf27ef16d03aa77e3dc1eee8b0c829ab
rahul <srahulsingh1999@gmail.com>
2025-07-05 00:23:41 +0530

    fixed preview

--- client/src/components/CartTShirtPreview.tsx
+++ client/src/components/CartTShirtPreview.tsx
@@ -106,8 +106,8 @@ const CartTShirtPreview: React.FC<CartTShirtPreviewProps> = ({
           top: '50%', 
           left: '50%', 
           transform: 'translate(-50%, -50%)', 
-          width: '35%', 
-          height: '35%',
+          width: '48%',
+          height: '48%',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center'
