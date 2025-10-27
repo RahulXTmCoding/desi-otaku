877de68480348af1df335c232c8e69fed14b136a
rahul <srahulsingh1999@gmail.com>
2025-10-27 16:16:26 +0530

    Fixed catalog

--- server/services/facebookCatalogService.js
+++ server/services/facebookCatalogService.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 const YOUR_CATALOG_ID = process.env.FACEBOOK_CATALOG_ID;
 const YOUR_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 const API_VERSION = 'v19.0';
-// --------------------------
 
 const API_URL = `https://graph.facebook.com/${API_VERSION}/${YOUR_CATALOG_ID}/items_batch`;
 
